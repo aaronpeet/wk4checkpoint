c0d6cbefcdbbc0425767eb2d0528b55d75cf038a
aaronpeet <aaronpeet@icloud.com>
2021-07-24 16:36:35 -0600

    temp math  done

--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -11,8 +11,16 @@ export default class Weather {
 
     get Template() {
         return `
-        <p>Temp: ${this.temp}</p>
+        <p>Temp: ${this.tempChangeC(this.temp)} &deg;C</p>
+        <p>Temp: ${this.tempChangeF(this.temp)} &deg;F</p>
         <p>City: ${this.name}</p>
         `
+    }
+     tempChangeF(value) {
+       return Math.floor((value-273.15)*1.8)+32
+     }
+    
+    tempChangeC(value) {
+        return Math.floor(value-273.15)
     }
 }
