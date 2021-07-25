5506fb86218ea297a1c2c2a7c68186d90f3179de
aaronpeet <aaronpeet@icloud.com>
2021-07-25 12:52:37 -0600

    clock controller finished

--- app/Controllers/ClockController.js
+++ app/Controllers/ClockController.js
@@ -1,28 +1,30 @@
 
+   function realtimeClock() {
+    
+        var clock = new Date()
+        
+        var hours = clock.getHours()
+        var minutes = clock.getMinutes()
+        var seconds = clock.getSeconds()
 
+        var amPm = (hours < 12) ? "AM" : "PM"
+        
+        hours = (hours > 12) ? hours - 12 : hours
+
+        hours = ("0" + hours).slice(-2)
+        minutes = ("0" + minutes).slice(-2)
+        seconds = ("0" + seconds).slice(-2)
+
+        document.getElementById('clock').innerHTML =
+            hours + "  :  " + minutes + "  :  " + seconds + " " + amPm
+        setTimeout(realtimeClock, 500)
+        
+}
 export default class ClockController{
     constructor() {
-        console.log('clock controller yay')
-        this.startTime()
+      
+        realtimeClock()
     }
 
-    startTime() {
-        const today = new Date();
-        setTimeout(() => {
-            let h = today.getHours();
-            let m = today.getMinutes();
-            let s = today.getSeconds();
-            m = this.checkTime(m);
-            s = this.checkTime(s);
-            document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
-         }, 1000);
-    }
-    
-     checkTime(i) {
-         if (i < 10) {
-             i = "0" + i
-         }
-         return i
-    }
     
 }
